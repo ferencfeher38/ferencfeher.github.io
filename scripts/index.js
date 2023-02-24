a476d9ee53efbbb7ac59d1bb8cebcaa777df8cf4
h982733 <h982733@stud.u-szeged.hu>
2023-02-24 09:44:36 +0100

    Add the final project  to the work section, small refactor

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,36 @@
-function HideScrollbar() {
-  var style = document.createElement("style");
+'use strict';
+
+const hideScrollbar = function () {
+  const style = document.createElement('style');
   style.innerHTML = `body::-webkit-scrollbar {display: none;}`;
   document.head.appendChild(style);
-}
+};
 
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+const myFunction = function () {
+  const x = document.getElementById('myTopnav');
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
   } else {
-    x.className = "topnav";
+    x.className = 'topnav';
   }
-}
+};
 
-function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
+const reveal = function () {
+  const reveals = document.querySelectorAll('.reveal');
 
   for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const elementVisible = 150;
 
     if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
+      reveals[i].classList.add('active');
     } else {
-      reveals[i].classList.remove("active");
+      reveals[i].classList.remove('active');
     }
   }
-}
+};
 
-window.addEventListener("scroll", reveal);
+window.addEventListener('scroll', reveal);
 
 reveal();
--- scripts/slides.js
+++ scripts/slides.js
@@ -1,28 +1,35 @@
+'use strict';
+
 let slideIndex = 1;
+
+const showSlides = function (n) {
+  let i;
+  let slides = document.getElementsByClassName('slides');
+  let dots = document.getElementsByClassName('demo');
+  let captionText = document.getElementById('caption');
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' active';
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+};
+
+const plusSlides = function (n) {
+  showSlides((slideIndex += n));
+};
+
+const currentSlide = function (n) {
+  showSlides((slideIndex = n));
+};
+
 showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("slides");
-    let dots = document.getElementsByClassName("demo");
-    let captionText = document.getElementById("caption");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    captionText.innerHTML = dots[slideIndex - 1].alt;
-}
